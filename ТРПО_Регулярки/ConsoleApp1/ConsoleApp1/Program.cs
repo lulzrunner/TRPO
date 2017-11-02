b9c252bc86c4c77956c04e23ff6fb6ef5041c49b
lulzrunner <kudesnik1337@outlook.com>
2017-11-02 22:22:38 +0300

    Регулярки
    Реализован поиск даты в текстовом файле с использованием регулярных выражений.

---
+++ ТРПО_Регулярки/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("in.txt");
+            StreamWriter fileOut = new StreamWriter("out.txt");
+            string text = file.ReadToEnd();
+            string[] words = text.Split(' ');
+            Regex regex = new Regex(@"(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d");
+            foreach (string word in words)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine("Найдена дата: " + word);
+                    fileOut.WriteLine(word);
+                }
+            }
+            file.Close();
+            fileOut.Close();
+            Console.ReadLine();
+        }
+    }
+}
